41e3cfe9d97593f20fa5e396cb2383ce766fe402
hubertc <>
2018-10-30 00:30:06 -0700

    new stuff

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws s3 rm s3://www.kejishoes.club --recursive
-aws s3 cp images s3://www.kejishoes.club/images --recursive
-aws s3 cp index.html s3://www.kejishoes.club/index.html
+#aws s3 rm s3://www.kejishoes.club --recursive
+aws s3 sync images s3://www.kejishoes.club/images --acl public-read
+aws s3 cp index.html s3://www.kejishoes.club/index.html --acl public-read
