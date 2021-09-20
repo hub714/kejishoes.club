f370b20f40f4fa106aa03c8572c304adcb9ce500
Hubertc <hubertcheung@gmail.com>
2021-09-20 16:38:18 -0700

    newest commit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #aws s3 rm s3://www.kejishoes.club --recursive
-aws s3 sync images s3://www.kejishoes.club/images --acl public-read
-aws s3 cp index.html s3://www.kejishoes.club/index.html --acl public-read
+aws s3 sync images s3://www.kejishoes.club/images --acl public-read --profile main
+aws s3 cp index.html s3://www.kejishoes.club/index.html --acl public-read --profile main
